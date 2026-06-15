b03f634886a8664959cbfafd51fdb1651244196a
muhammadRizki12 <m.rizkil7621@gmail.com>
2026-06-15 15:49:39 +0700

    update table ADMIN_USER to admin_user

--- src/services/auth/repositories/auth-repositories.js
+++ src/services/auth/repositories/auth-repositories.js
@@ -3,8 +3,8 @@ import pool from '../../../config/db.js';
 class AuthRepository {
   async findByCredentials(username, password) {
     const [rows] = await pool.query(
-      'SELECT * FROM ADMIN_USER WHERE username = ? AND password = ?',
-      [username, password]
+      'SELECT * FROM admin_user WHERE username = ? AND password = ?',
+      [username, password],
     );
     return rows[0] || null;
   }
